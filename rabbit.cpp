9189c4d0c7f2d19b8b522e8035708fd6574cec7c
Pere <perebercerotrigell@gmail.com>
2025-10-05 17:13:02 +0200

    initial commit

---
+++ rabbit.cpp
@@ -0,0 +1,13 @@
+
+// Write code below 💖
+
+#include <iostream>
+int main () {
+std::cout << "|￣￣￣￣￣￣￣|\n" ;
+std::cout << "|   MESSAGE   |\n" ; 
+std::cout << "|    HERE     |\n" ;
+std::cout << "|_____________|\n" ;
+std::cout << "(\\__//) ||\n" ; 
+std::cout << "(•ㅅ•) ||\n" ;
+std::cout << "/ 　 づ\n" ;
+}
