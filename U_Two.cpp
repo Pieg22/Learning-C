185922f2821adfcac42cedb1bb87e3b5f33a88b5
Pere <perebercerotrigell@gmail.com>
2025-10-11 19:16:43 +0200

    update

---
+++ U_Two.cpp
@@ -0,0 +1,33 @@
+// This is the first exercice in C++ for Roberto 
+
+/*
+1.El departamento técnico se ha propuesto desarrollar una aplicación para
+calcular la media de tres valores. Te han pedido que analices el problema. Realiza la tarea de análisis. */
+
+#include <cmath>
+int main () {
+    int  3 ; 
+    int  4 ; 
+     
+}
+
+
+
+
+/*
+2. Diseña el algoritmo, en pseudocódigo o diagrama de flujo, que permita calcular
+la media en base al análisis del ejercicio anterior.
+3. Diseña e implementa un programa que calcule el área de un triángulo.
+4. Implementa un programa que, dado radio de una circunferencia, calcule su
+perímetro.
+5. Implementa un programa que gestiona la partida presupuestaria de un colegio
+internacional. Se deben introducir los ingresos en euros, dólares y rublos, y el
+programa debe indicar, en euros, cuánto dinero se destina al profesorado (60%
+del total), cuánto dinero se dedica a mejorar las instalaciones (20% del total) y
+cuánto a otros gastos (el resto).
+6. Se desea implementar un programa que calcule la hora en la que una olla de
+cocción lenta terminará de cocinar una receta. Para ello, hay que introducir la
+hora actual y el tiempo (en segundos) de cocción que necesita la receta. El
+programa debe indicar la hora a la que estará lista
+*/
+
