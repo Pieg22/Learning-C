042660fd5abdffd225e5ce7ce2ff84a300f1ea77
Pere <perebercerotrigell@gmail.com>
2025-10-23 21:15:56 +0200

    update

--- Unidad_2/colegio.cpp
+++ Unidad_2/colegio.cpp
@@ -4,8 +4,16 @@ programa debe indicar, en euros, cuánto dinero se destina al profesorado (60%
 del total), cuánto dinero se dedica a mejorar las instalaciones (20% del total) y
 cuánto a otros gastos (el resto). */ 
 #include <iostream>
+#include <string>
 int main () {
-    double ingresos ;
+    double ingresos_e = 100 ;
+    double exchange_1 = ingresos_e * 1.16 ; 
+    double exchange_2 = ingresos_e * 94.58 ;
+    double teachers = (ingresos_e * 60) / 100 ;  //Dinero destinado al profesorado 
+    double instalations = (ingresos_e * 20) / 100 ; //Dinero destinado a las instalaciones 
+    double resto = (ingresos_e * 20) / 100 ; //Dinero destinado a otros gastos 
+    
+    std::cout << "El dinero destinado a los profesores asciende a : "<<teachers<< "\n"  "Quedando : "<<instalations<<" destinado a instalaciones " \n" Y "<<resto<<" para otros gastos "; 
     
 
 
