1543ec6fab09cefb31720a375c4782b8c188e44a
Pere <perebercerotrigell@gmail.com>
2025-10-25 19:12:22 +0200

    update

--- Unidad_2/Circunferencia.cpp
+++ Unidad_2/Circunferencia.cpp
@@ -1,18 +1,15 @@
 // 4. Implementa un programa que, dado radio de una circunferencia, calcule su perímetro . 
-#define _USE_MATH_DEFINES //Ojo he encontrado la libreria oculta para utilizar PI! https://cplusplus.com/forum/general/102410/
+//#define _USE_MATH_DEFINES //Ojo he encontrado la libreria oculta para utilizar PI! https://cplusplus.com/forum/general/102410/
 
 #include <iostream> // Importamos la libreria de comunicación 
-#include <cmath>  // También importe la de calculos math , pensando que estaría inlcuido el número pi , pero ya he visto que no ! Esto no es Python hahahah
 int main () {     // Empieza nuestro programa principal 
-    double radio ;  // Variable nombrada radio , con data type double 
+    int radio ;  // Variable nombrada radio , con data type double 
     std::cout << "Introduce el radio de tu circunferencia : " ; // Clasico Input para preguntar el valor del radio de la circunferencia 
     std::cin >> radio ;  // Guardamos aquí el valor que nos dió el user en el paso anterior 
+    std::cout << "El perímetro de tu circunferencia es :"<< radio * radio  * 3.1415<< " \n " ;  // Print con un string y el cálculo del perimetro de la circunferencia utilizando el formateo dentro del string
+    
 
-    if (radio > 0) {   // Condicional afirmando que si el radio es un número positivo realize su interior . De lo contrario , no seguirá el programa . 
-        std::cout << "El perímetro de tu circunferencia es :"<< radio * radio  * M_PI << " \n " ;  // Print con un string y el cálculo del perimetro de la circunferencia utilizando el formateo dentro del string
-    } 
-
- return 0; // el programa retorna 0 si a funcionado todo bien , de lo contrario saldría un nonzero , para indicar que a fallado el programa . En el "Output" podemos observarlo 
+return 0; // el programa retorna 0 si a funcionado todo bien , de lo contrario saldría un nonzero , para indicar que a fallado el programa . En el "Output" podemos observarlo 
 
 }
 
