64c28a7f62c96465bc649a69ac382c39a9e1ccc7
Pere <perebercerotrigell@gmail.com>
2025-11-07 18:40:21 +0100

    update

--- Unidad_2/Triangle.cpp
+++ Unidad_2/Triangle.cpp
@@ -5,14 +5,18 @@
 #include <iostream> //Importamos la libreria con la que podemos interactuar con la terminal 
 int main () {       //Aquí empieza nuestro programa 
     
-    int base , high , calculo;      // Creamos la variable nombrada base y high con su data type int , para guardar la base y altura  del triángulo. 
+    float base , high , calculo;      // Creamos la variable nombrada base y high con su data type int , para guardar la base y altura  del triángulo. 
+
     std::cout << "Please introduce the base of the triangle and high in same order with space betwen:  " ; //Creamos un print en la terminal para que el user introduzca la base del triángulo
-    std::cin >> base , high ;  // Utilizamos la variable base y high , para guardar el valor del anterior input
+
+    std::cin >> base >> high ;  // Utilizamos la variable base y high , para guardar el valor del anterior input 
+
     calculo = (base * high) / 2 ; //Se realiza el calculo del área
+    
     std::cout << "The area of your triangle is "<< calculo << "\n" ;
    
     return 0 ; }  // Si todo va bien , el programa retorna 0 . De lo contrario reotrna a veces un 1 para indicar que a fallado el programa . En el "output" podemos verlo 
 
 
-
+// std::cin >> base >> high ; std::cin >> base , high ; !!! Importante . 
 
--- Unidad_2/colegio.cpp
+++ Unidad_2/colegio.cpp
@@ -4,20 +4,33 @@ programa debe indicar, en euros, cuánto dinero se destina al profesorado (60%
 del total), cuánto dinero se dedica a mejorar las instalaciones (20% del total) y
 cuánto a otros gastos (el resto). */ 
 #include <iostream>
+using namespace std ; 
 
+const float exchange_1  = 0.86 ; //Constante para dolares
+const float exchange_2 = 0.011 ; // Constante para rublos
 int main () {
-    int ingresos_e = 160 ; // Entrada de euros
-    int ingresos_r = 3500 ; //entrada de rublos
-    int ingresos_d = 6500 ;  //entrada de dolares
-    float exchange_1 = ingresos_d * 0.86 ; // Cambio de dolares a euros
-    float exchange_2 = ingresos_r * 0.011 ; // Cambio de rublos a euros  
-    int suma_total = ingresos_e + exchange_1 + exchange_2 ; // Suma total de los ingresos 
-    int teachers = (suma_total * 60) / 100 ;  //Dinero destinado al profesorado euros
-    int instalations = (suma_total * 20) / 100 ; //Dinero destinado a las instalaciones euros
-    int resto = (suma_total * 20) / 100 ; //Dinero destinado a otros gastos  euros 
+    float ingresos_e , ingresos_r , ingresos_d ; // Variables para
+
+    cout << "Introduce el dinero en euros : " ; 
+    cin >> ingresos_e ; 
+
+    cout << "Introduce la cantidad de rublos : " ; 
+    cin >> ingresos_r ; 
+
+    cout << "Introduce la entrada de doalres : " ; 
+    cin >> ingresos_d ; 
+
+    float exchange_dolares = ingresos_d * exchange_1  ; // Cambio de dolares a euros
+    float exchange_rublos = ingresos_r * exchange_2  ; // Cambio de rublos a euros  
+
+    float suma_total = ingresos_e + exchange_1 + exchange_2 ; // Suma total de los ingresos 
+    
+    float teachers = (suma_total * 60) / 100 ;  //Dinero destinado al profesorado euros
+    float instalations = (suma_total * 20) / 100 ; //Dinero destinado a las instalaciones euros
+    float resto = (suma_total * 20) / 100 ; //Dinero destinado a otros gastos  euros 
 
     
-    std::cout << "El dinero destinado a los profesores asciende a : "<<teachers<< " euros , quedando  "<<instalations<<" euros destinado a instalaciones y "<<resto<<" euros  para otros gastos "; 
+    cout << "El dinero destinado a los profesores asciende a : "<<teachers<< " euros , quedando  "<<instalations<<" euros destinado a instalaciones y "<<resto<<" euros  para otros gastos "; 
     
 
 
