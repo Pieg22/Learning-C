84c6696774ca89f0bf0e65df345b5fce730ecc63
Pieg.22 <perebercerotrigell@gmail.com>
2025-11-30 11:45:56 +0100

    Comentado

--- Unidad_4/Ejercicio_2.cpp
+++ Unidad_4/Ejercicio_2.cpp
@@ -5,31 +5,31 @@ indicada, haciendo uso de las funciones de la librería string. Controla los pos
 errores.
 El string aquí es mágico y ya es un array a su mismo tiempo  */
 
-#include <iostream>
-#include <string>
-using namespace std ; 
+#include <iostream> // Libreria clasica
+#include <string> // Introducimos nueva libreria que nos da paso a trabajar con multitud de tools para strings 
+using namespace std ;  // std :: 
 
-int main () {
+int main () { // Arrancamos 
 
-    string cadena , caracter ; 
-    int cadena_longitud , insertar_posicion ; 
+    string cadena , caracter ;  // Creamos las variables de string para guardar nuestros valores más tarde 
+    int cadena_longitud , insertar_posicion ;  // Creamos variables numericas  con las que trabajaremos la insercion de el caracter 
 
-    cout << " Escribe una cadena de texto :  " << endl ; 
-    getline(cin, cadena )  ; 
+    cout << " Escribe una cadena de texto :  " << endl ;  // Pedimos al user la cadena 
+    getline(cin, cadena )  ;                              // Guardamos la cadena anterior , pero con getline importante , ya que cin aquí no funciona bien ! Con los espacios y el enter se vuelve loco .
     
-    cout << "Escribe un caracter : " <<endl ; 
-    getline (cin,caracter) ;
+    cout << "Escribe un caracter : " <<endl ;            // Pedimos el caracter
+    getline (cin,caracter) ;                             // Guardamos el caracter
+ 
 
-
-    cadena_longitud = cadena.length() ; 
+    cadena_longitud = cadena.length() ;                  //Creamos la variable y le damos como valor la longitud de la cadena para poder trabajar con ella 
     
-    cout << "En que posicion de la cadena " << cadena_longitud << " quieres introducir el caracter ? " <<endl ;
-    cin >> insertar_posicion ; 
+    cout << "En que posicion de la cadena " << cadena_longitud << " quieres introducir el caracter ? " <<endl ; // Imprimimos  la cantidad de length para mejor visualizacion
+    cin >> insertar_posicion ;  // cojemos la posicion numerica donde se quiere insertar el caracter 
 
-    if (insertar_posicion > cadena_longitud) cout << " Error , la longitud introducida no pertenece a la cantidad de la cadena " << endl ; 
-    else cadena.insert(insertar_posicion,caracter) ;
+    if (insertar_posicion > cadena_longitud) cout << " Error , la longitud introducida no pertenece a la cantidad de la cadena " << endl ;  // Nos adelantamos a un posible error 
+    else cadena.insert(insertar_posicion,caracter) ; // Si pasa el control del error , es hora de insertat el caracter en la posicion que nos indica el user 
 
-    cout << cadena ; 
+    cout << cadena ;  // Imprimios el resultado final de la cadena con el caracter insertado en la posicion desdeada . 
 
     return 0 ; 
 }
