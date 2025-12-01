96b00eb2abc7d83ed376b9b2b4b03e027fd10a6e
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-01 06:55:58 +0100

    Start w 4

--- Unidad_4/Ejercicio_4.cpp
+++ Unidad_4/Ejercicio_4.cpp
@@ -1,3 +1,40 @@
 /*Implementa un programa que pida los elementos de una matriz de tamaño no
 superior a 10x10 e indique si la matriz es diagonal simétrica, triangular superior y/o
-la matriz identidad.*/
+la matriz identidad.
+ -Para ver simetria , primeramente hay que trasponerla y ver si los elementos que crean la diagional , al trasponerla estan en el mismo sitio o no , buscaremos que no esten en el mismo 
+sitio . Una matriz cuadrada siempre tendrá la diagonal identica  tanto la original como la traspuesta . 
+ - para triangular superior necesitamos saber si la parte inferior de la diagional , esta llena de 0 ( U = upper triangular matrix) 
+ */
+
+#include <iostream>
+using namespace std ; 
+
+const int nmax = 10 ; 
+
+int main (){
+    float matrix [nmax][nmax] ;
+    int nuser ; 
+    bool diagonal = true ; 
+
+    cout << "Introduce el numero de filas de la matriz cuadrada (<10) : "<<endl;
+    cin >> nuser;
+
+    for(int i = 0 ; i < nuser ; i++){
+        for (int j = 0; j < nuser; j++)
+        {
+         cout<< "Introduce el elemento i "<< i+1 << " y j " << j+1 <<endl; 
+         cin >> matrix[i][j] ; 
+        }
+
+     for(int i = 0 ; i < nuser ; i++){
+        for (int j = 0; j < nuser; j++)
+        {
+         if (matrix[-i -1] != 0)
+
+
+        }
+        
+    }
+    return 0 ; 
+}
+
