1a24214f86c39cb0220eb829b8904187b92996c1
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-01 21:06:22 +0100

    Start W7

--- Unidad_4/Ejercicio_7.cpp
+++ Unidad_4/Ejercicio_7.cpp
@@ -1,3 +1,45 @@
 /*Mejora el programa anterior para que permita introducir hasta un máximo de
 100 libros. Además del introducido por el usuario, introduce por código dos libros
 más.*/
+
+#include <iostream> // Libreria de persona a consola , consola a persona 
+#include <string> // Para utilizar las funciones de getline conjunto con ws 
+using namespace std ; // std:: 
+
+const int maxlibros = 100 ; 
+
+
+int main (){
+    struct database {  // nombramos nuestra estructura de datos  para guardar la informacion sobre los libros
+        string title ;  // declaramos una variable string para el titulo
+        string writter ;  // variable autor 
+        int year ;  // variable de enteros para  el año
+        float precio ; //  variable con decimales para el precio
+    } ;  // Algo muy importante en este caso con las estructuras es necesario poner ; después del } , si no se rompe todo ! 
+
+    database libros ;  // montamos nuestra esctructura de datos  encima de "libros" , de esta manera tenemos facil acceso a la misma . 
+    
+// A partir de aquí hacia abajo , simplemente pedimos datos , guardamos datos , y finalmente imprimimos los datos de nuevo . 
+    cout << "Introduce el titulo del libro : " <<endl; 
+    getline(cin >> ws, libros.title);
+
+    cout << "Quien es el escritor/ra ? " <<endl;
+    getline(cin >> ws, libros.writter);
+
+    cout << "De que año es el libro ? " <<endl;
+    cin >> libros.year ; 
+
+    cout << "Que precio tiene el tomo ? " <<endl;
+    cin >> libros.precio ; 
+
+    cout << "Caracteristicas del libro : " << "\n" 
+            " - Titulo : " <<libros.title<< "\n"
+            " - Autor : " <<libros.writter<< "\n"
+            " - Año : " <<libros.year<< "\n"
+            " - Precio : " <<libros.precio<< endl; 
+
+
+
+
+    return 0; 
+}
