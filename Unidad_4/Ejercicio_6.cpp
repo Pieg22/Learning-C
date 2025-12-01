f2a59c96748e2e256dfc137745c008a112c04d04
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-01 20:59:42 +0100

    Sobraban datos

--- Unidad_4/Ejercicio_6.cpp
+++ Unidad_4/Ejercicio_6.cpp
@@ -2,43 +2,38 @@
 qué tipo de estructura de datos debería implementar un libro y realiza un
 programa que lea los datos del teclado y los muestre por pantalla.*/
 
-#include <iostream>
-#include <string>
-using namespace std ; 
+#include <iostream> // Libreria de persona a consola , consola a persona 
+#include <string> // Para utilizar las funciones de getline conjunto con ws 
+using namespace std ; // std:: 
 
 
 int main (){
-    struct database {
-        string title ; 
-        string category ;
-        string writter ;  
-        int npages ; 
-        float precio ; 
-    } ; 
+    struct database {  // nombramos nuestra estructura de datos  para guardar la informacion sobre los libros
+        string title ;  // declaramos una variable string para el titulo
+        string writter ;  // variable autor 
+        int year ;  // variable de enteros para  el año
+        float precio ; //  variable con decimales para el precio
+    } ;  // Algo muy importante en este caso con las estructuras es necesario poner ; después del } , si no se rompe todo ! 
 
-    database libros ; 
+    database libros ;  // montamos nuestra esctructura de datos  encima de "libros" , de esta manera tenemos facil acceso a la misma . 
     
-
-    cout << "Introduce el titulo del libro : " <<endl;
+// A partir de aquí hacia abajo , simplemente pedimos datos , guardamos datos , y finalmente imprimimos los datos de nuevo . 
+    cout << "Introduce el titulo del libro : " <<endl; 
     getline(cin >> ws, libros.title);
-
-    cout << "Escribe la categoria del libro : " <<endl;
-    getline(cin >> ws, libros.category );
 
     cout << "Quien es el escritor/ra ? " <<endl;
     getline(cin >> ws, libros.writter);
 
-    cout << "Cuantas paginas tiene el libro ? " <<endl;
-    cin >> libros.npages ; 
+    cout << "De que año es el libro ? " <<endl;
+    cin >> libros.year ; 
 
     cout << "Que precio tiene el tomo ? " <<endl;
     cin >> libros.precio ; 
 
     cout << "Caracteristicas del libro : " << "\n" 
             " - Titulo : " <<libros.title<< "\n"
-            " - Categoria : " <<libros.category<< "\n"
             " - Autor : " <<libros.writter<< "\n"
-            " - Nº paginas : " <<libros.npages<< "\n"
+            " - Año : " <<libros.year<< "\n"
             " - Precio : " <<libros.precio<< endl; 
 
 
