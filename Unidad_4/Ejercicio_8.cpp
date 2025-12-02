07ea3ce1fbb9315b0b03eaefd1e23c6b7547cf53
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-02 06:52:31 +0100

    8 done

--- Unidad_4/Ejercicio_8.cpp
+++ Unidad_4/Ejercicio_8.cpp
@@ -1,2 +1,24 @@
 /*Realiza un programa que pida una cadena de texto al usuario, invierta la
 cadena y la muestre por pantalla*/
+
+#include <iostream>
+#include <string>
+using namespace std ; 
+
+int main () {
+    string cadena ;  // string es como un array de caracteres
+    int longitud ; 
+
+    cout << "Introduce una cadena de texto (19 caracteres max) : " <<endl; 
+    getline(cin >> ws, cadena) ; 
+
+    longitud = cadena.length() ; 
+
+
+    for ( int i = 20 ;  i > -1 ; i--) cout << cadena[i];
+
+    
+
+
+    return 0 ;
+}
