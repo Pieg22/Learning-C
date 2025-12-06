65514840981c1b521c11ba057107973dd19c46a4
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-06 15:41:46 +0100

    Understood

--- Unidad_4/Ejercicio_4.cpp
+++ Unidad_4/Ejercicio_4.cpp
@@ -14,7 +14,7 @@ const int nmax = 10 ;  // Constante declarada para el maximo de la dimension de 
 int main (){
     float matrix [nmax][nmax] ; // Declaramos un array para la matriz , añadiendo su constante en  los dos parametros 
     int nuser ;  // variable que utilizaremos para pedir al user la dimension de la matriz 
-    bool diagonal = true  , triangular = true ; // nuestras banderas para realizar las comprobaciones que pide el enunciado 
+    bool diagonal = true  , triangular = true , identidad = true; // nuestras banderas para realizar las comprobaciones que pide el enunciado 
 
     cout << "Introduce el numero de filas de la matriz cuadrada (<10) : "<<endl; // pedimos datos al user en consola 
     cin >> nuser; // Guardamos el valor de la dimension de la matriz que creara el user 
@@ -32,19 +32,28 @@ int main (){
         {
          if (matrix[i][j] != matrix[j][i]) {diagonal = false ; } ; // Si la matriz original es diferente a la espejo , la diagonal no va a ser igual , por lo tanto no será diagonal simetrica
 
-         if ( i > j  && matrix[i][j] != 0 ) {triangular = false ; } ; // i > j , comprende que bajamos hasta la parte inferior de la matriz , y ademas , si NO encontramos un 0  , no sera 
-                                                                      // una matriz triangular! 
+         if ( i > j  && matrix[i][j] != 0 ) {triangular = false ; } ; // i > j , compara la posicion de i y j . Es decir si para i a recorrido 1 posicion y para j 2 posiciones por ej . 
+                                                                      // con lo que quiere decir que cada vez que i sea mayor que j , es que estamos por debajo de la diagonal de la matriz  ( ej i == 2 , j==1)
+                                                                      // desde esta primera posición hacia abajo empieza a construir el triangulo inferior y si encuentra algo != de 0  . Cambia la bandera ofc .
+         if(i == j  ) { // Comprobamos si estamos en la posicion diagonal primeramente 
+            if ( matrix[i][j] != 1 ) {identidad = false ; } ; // Una vez verificamos que estamos en la diagonal , comprobamos si tenemos cualquier valor que NO sea un 1 
+         } 
+         else { if (matrix[i][j] != 0 ) identidad = false ;} // Estando fuera de la diagonal , comprobamos que en la posicion actual no haya nada que NO sea un 0 ! 
+                                                                    
 
 
         }
         
     }
 
-    if(diagonal == true ) cout << "La matriz es diagonal simetrica. " <<endl; // Comprobamos bandera diagonal 
+    if(diagonal == true ) cout << " La matriz es diagonal simetrica. " <<endl; // Comprobamos bandera diagonal 
     else cout << "La matriz no es diagonal simetrica . " <<endl;
 
-    if ( triangular == true ) cout << "La matriz es triangular superior " <<endl; // comprobamos bandera triangular 
+    if ( triangular == true ) cout << " La matriz es triangular superior " <<endl; // comprobamos bandera triangular 
     else cout << "La matriz no es triangular superior . " <<endl;
+
+    if( identidad == true ) cout << " La matriz si es identidad . "<<endl;
+    else cout<< "La matriz no es identidad."<<endl;
 
 
 
