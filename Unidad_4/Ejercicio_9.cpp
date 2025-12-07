a52e732fdb724f86a987f8cb692047d35389741b
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-07 10:03:59 +0100

    trying to complete

--- Unidad_4/Ejercicio_9.cpp
+++ Unidad_4/Ejercicio_9.cpp
@@ -2,7 +2,8 @@
 cadena es más corta que el vector, se debe reemplazar el contenido del vector
 por cada carácter de la cadena (hasta llegar al final de la cadena). Si es al
 contrario, se deben reemplazar los caracteres de la cadena por los números del
-vector*/
+vector
+No podemos utilizar swap porque las variables son de diferentes longitudes , y para que swap funcione , tienen que ser identicos en longitud */
 
 
 #include <iostream>
@@ -13,35 +14,35 @@ const int TAM = 5 ;
 
 int main (){
 
-     int numeros[TAM] , longitud_cadena ; 
-     string cadena , intercambio  ; 
+     string numeros[TAM] ; 
+     int longitud_cadena ; 
+     string cadena ;
 
     for (int  i = 0 ; i < TAM ; i++){
         
         cout<< "Introduce un numero entero del 1 al 20 : " <<endl;
-        cin >> numeros[i] ;    
-    }
+       getline( cin >>  ws , numeros[i] ); 
+    } 
 
     cout << "Introduce una cadena de texto : "<<endl;
     getline(cin , cadena ); 
 
      longitud_cadena = cadena.length() ;
 
-    if (longitud_cadena > TAM) {
-        for ( int k = 0 ; k < longitud_cadena ; k++) {
-           
+    if (longitud_cadena < TAM) {
+        for ( int k = longitud_cadena ; k < TAM ; k++) {
+            cadena.replace(k, 5 , numeros[k]) ; 
+            cout << " " << cadena <<endl; 
 
-
-            
-
-
-        }
+        } ; 
     }
+    else if (longitud_cadena > TAM){
+        for ( int q = 0 ; q < TAM ; q++) {
+            cadena.replace(q, 1 , numeros[q]) ; 
+            cout << " " << cadena <<endl; 
+        } ; 
 
-
-     
-
-
-
+    }
+        
     return 0 ; 
 }
