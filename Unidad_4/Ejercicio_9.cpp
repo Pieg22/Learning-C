816cdd35cdb898657b0894a3087d77094351b9ca
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-07 15:08:34 +0100

    done/commented

--- Unidad_4/Ejercicio_9.cpp
+++ Unidad_4/Ejercicio_9.cpp
@@ -6,41 +6,49 @@ vector
 No podemos utilizar swap porque las variables son de diferentes longitudes , y para que swap funcione , tienen que ser identicos en longitud */
 
 
-#include <iostream>
+#include <iostream> 
 #include <string>
-using namespace std ; 
+using namespace std ;  
 
-const int TAM = 5 ;
+const int TAM = 5 ; // Constante que utilizaremos para los strings
 
 int main (){
 
-     string numeros[TAM] ; 
-     int longitud_cadena ; 
-     string cadena ;
+     string numeros[TAM] ;  // Variable string con 5 cajones
+     int longitud_cadena ; // variable que utilizaremos de contador 
+     string cadena ;       // Variable sin cajones pero como array 
 
-    for (int  i = 0 ; i < TAM ; i++){
+    for (int  i = 0 ; i < TAM ; i++){  // Funciona hasta recibir las 5 entradas 
         
-        cout<< "Introduce un numero entero del 1 al 20 : " <<endl;
-       getline( cin >>  ws , numeros[i] ); 
+        cout<< "Introduce un numero entero del 1 al 20 : " <<endl; // Pedimos al user las entradas
+       getline( cin >>  ws , numeros[i] );  // Guardamos cada entrada , con getline para que no se contamine el programa y detecte un enter falso y se sale las siguientes lineas 
     } 
 
-    cout << "Introduce una cadena de texto : "<<endl;
-    getline(cin , cadena ); 
+    cout << "Introduce una cadena de texto : "<<endl; // Pedimos al user la cadena 
+    getline(cin , cadena );  //Guardamos la cadena en su variable
 
-     longitud_cadena = cadena.length() ;
+     longitud_cadena = cadena.length() ; // Medimos la cadena  , para así traducirla a int y poder comparlarla 
 
-    if (longitud_cadena < TAM) {
-        for ( int k = longitud_cadena ; k < TAM ; k++) {
-            cadena.replace(k, 5 , numeros[k]) ; 
-            cout << " " << cadena <<endl; 
+    if (longitud_cadena < TAM) { // Si pepe es menor que 5
+        for ( int k = longitud_cadena ; k < TAM ; k++) { // Empezamos a añadir cosas después de la cadena ya que le estamos dando el valor de "longitud_cadena " a k . 
+                                                         // de la misma forma meteremos números , hasta que k sea igual de larga que TAM . 
+            cadena += numeros[k] ;  // concatenamos  porque necesitamos añadir más cosas ( numeros en este caso ) , es decir , enlazamos los characteres con los numeros que entren hasta llegar
+                                    // al mismo tamaño que k . 
+            
 
         } ; 
+
+        cout << " " << cadena <<endl; // Imprimimos la cadena fuera del bucle , después de haber sido "formateada"
     }
-    else if (longitud_cadena > TAM){
-        for ( int q = 0 ; q < TAM ; q++) {
-            cadena.replace(q, 1 , numeros[q]) ; 
-            cout << " " << cadena <<endl; 
+    else if (longitud_cadena > TAM){ // Si paquito ( 7 leters) > TAM (5) 
+        for ( int q = 0 ; q < TAM ; q++) { //  // En este bucle empezamos por 0 , ya que lo que queremos es sustituir las letras por los numeros , por lo tanto queremos empezar desde 
+                                               // el principio de la cadena , ( posicion 0 ofc ). 
+            cadena.replace(q, 1 , numeros[q]) ; // indicamos a replace que empieze desde la posición 'q' , el 1 es la cantidad de objetos que queremos reemplazar en cada ronda 
+                                                // numeros[q] coje lo que se encuentre en ese momento en "numeros" para reemplazarlo por lo que este dentro de ese cajon ! 
+            
         } ; 
+        
+        cout << " " << cadena <<endl; 
 
     }
         
