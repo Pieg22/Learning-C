7bd83e84ebec0316a1a8fe86223c3e8b68ba7e68
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-23 12:35:11 +0100

    Process

--- Unidad_5/Ejercicio_1.cpp
+++ Unidad_5/Ejercicio_1.cpp
@@ -1,3 +1,50 @@
 /*Realiza una función que, dado un vector de N números enteros, devuelva la
 posición del primer cero que encuentre en el vector.*/
 
+#include <iostream>
+
+using namespace std;
+
+const int N_numeros = 100 ; 
+
+int main (){
+
+    int numeros[N_numeros] ; 
+    int recogida ; 
+
+    cout << "Cuantos numeros quieres introducir ? " <<endl ; 
+    cin >> recogida ; 
+
+     
+    
+    for ( int i = 0  ; recogida > i ; i++) {
+        
+        cout << " Introduce un numero por favor : " <<endl ; 
+        cin >> numeros[i] ; 
+
+    } 
+
+         
+
+    
+   
+    
+    return 0; 
+}
+
+
+int Find_the_position (int numeros[], int tamanio){
+
+    for (int k = 0 ; k < tamanio ; k++){
+
+        if (numeros[k] == 0 ){
+            cout << "La posicion en la que se encuentra el 0 es : " << k <<endl;  
+
+            return k ; 
+        }
+
+    }
+    cout << "No se ha encontrado ningun 0." << endl;
+    return -1; 
+  
+} ; 
