e5e2352180e17d28f6c7866a08f42bb93789de5a
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-27 18:15:11 +0100

    contaminated result

--- Unidad_5/Ejercicio_2.cpp
+++ Unidad_5/Ejercicio_2.cpp
@@ -8,12 +8,12 @@ int calculo(int n1, int n2) ;
 
 int main (){
     int n1 , n2 ; 
-    float result ; 
+     
 
     cout << "Introduce dos numeros separados por un espacio para comprobar si son divisibles : "<<endl;
     cin >> n1 >> n2 ; 
 
-    cout<< "El resultado del calculo es : "<< result <<endl; 
+    cout<< "El resultado del calculo es : "<< calculo <<endl; 
     
     int resultado = calculo (n1,n2);
 
@@ -22,7 +22,7 @@ int main (){
 
 int calculo(int n1, int n2){
 
-   float result = n1 / n2 ; 
+   int result = n1 / n2 ; 
     if ( n1 % n2 == 0) return 0 ;   
     else if ( n1 % n2 != 0) return -1 ; 
 
