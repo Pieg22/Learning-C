e03084fa924ef2088fd73e6c603d37da9414d8ce
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-27 17:59:28 +0100

    process

--- Unidad_5/Ejercicio_2.cpp
+++ Unidad_5/Ejercicio_2.cpp
@@ -1,2 +1,26 @@
 /*Realiza una función ( un programa completo ) que diga si un número es divisible por otro ( es decir que el resultado sea un n entero 
 y que el resto sea 0 ).*/ 
+
+#include <iostream>
+using namespace std ; 
+
+int calculo(int n1, int n2) ; 
+
+int main (){
+    int n1 , n2 ; 
+
+    cout << "Introduce dos numeros separados por un espacio para comprobar si son divisibles : "<<endl;
+    cin >> n1 >> n2 ; 
+
+    cout<< "El resultado del calculo es : "<< n1 / n2 <<endl; 
+    
+    int resultado = calculo (n1,n2);
+
+    return 0 ; 
+}
+
+int calculo(int n1, int n2){
+
+    if ( n1 % n2 == 0) return 0 ;  
+
+}
