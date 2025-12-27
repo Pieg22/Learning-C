26b11757869da07be651d5f9caf66ef887868f80
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-27 20:27:26 +0100

    Just copy the fuctions from teacher ofc

--- Unidad_5/Ejercicio_3.cpp
+++ Unidad_5/Ejercicio_3.cpp
@@ -3,3 +3,27 @@ ello, se utilizará su forma polar. El usuario introducirá los números en form
 cartesiana y después introducirá la operación a realizar (multiplicación o
 división). El programa mostrará la solución tanto en formato cartesiano como en
 formato polar.*/
+ 
+#include <iostream>
+#include <cmath>
+using namespace std ; 
+
+#define PI 3.141593 
+
+int main (){
+    return 0; 
+}
+
+float modulo (float x,float y){
+    
+    return sqrt(x*x + y*y);
+}
+
+float fase (float x , float y){
+    float angulo ; 
+
+    if(y > 0) angulo = atan2(y,x) * 180.00/PI ; 
+    else angulo atan2(y,x) * 180.0/PI + 360.0 ; 
+
+    return angulo ; 
+}
