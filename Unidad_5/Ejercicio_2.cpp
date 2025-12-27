38c7ce8b51037918554a76f6b099a2ea3366794c
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-27 18:44:01 +0100

    trying bool system

--- Unidad_5/Ejercicio_2.cpp
+++ Unidad_5/Ejercicio_2.cpp
@@ -4,27 +4,30 @@ y que el resto sea 0 ).*/
 #include <iostream>
 using namespace std ; 
 
-int calculo(int n1, int n2) ; 
+bool calculo(int n1, int n2) ; 
 
 int main (){
     int n1 , n2 ; 
      
-
     cout << "Introduce dos numeros separados por un espacio para comprobar si son divisibles : "<<endl;
     cin >> n1 >> n2 ; 
 
-    int resultado = calculo (n1,n2);
+    bool resultado = calculo (n1,n2);
 
-    cout<< "El resultado del calculo es : "<< resultado <<endl; 
+    if ( resultado == true ) cout << "Los numeros son divisibles "<<endl; 
+    else cout<< "Los numeros no son divisibles. "<<endl; 
+
+
     
 
     return 0 ; 
 }
 
-int calculo(int n1, int n2){
+bool calculo(int n1, int n2){
 
    int result = n1 / n2 ; 
-    if ( n1 % n2 == 0) return 0 ;   
-    else if ( n1 % n2 != 0) return -1 ; 
+   cout << "El resultado de la division es " << result <<endl; 
+    if ( n1 % n2 == 0) true ;   
+    else false ; 
 
 }
