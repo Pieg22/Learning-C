611012c5b2aba1e74946bb581125f6e61bfbaaca
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-27 17:45:05 +0100

    starting

--- Unidad_5/Ejercicio_2.cpp
+++ Unidad_5/Ejercicio_2.cpp
@@ -1,1 +1,2 @@
-/*Realiza una función que diga si un número es divisible por otro.*/
+/*Realiza una función ( un programa completo ) que diga si un número es divisible por otro ( es decir que el resultado sea un n entero 
+y que el resto sea 0 ).*/ 
