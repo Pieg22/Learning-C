952585f1dae859cebddab166355ee08ab54c6812
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-30 13:29:30 +0100

    Start the structure

--- Unidad_5/Ejercicio_4.cpp
+++ Unidad_5/Ejercicio_4.cpp
@@ -1,2 +1,28 @@
 /*Realiza una función que, usando la función del ejercicio 2, nos indique si un
 número es primo o no.*/
+#include <iostream>
+using namespace std; 
+
+bool calculo(int, int) ; 
+
+int main (){
+    int n , ; 
+    bool primo  = true , comprobacion  ; 
+
+    cout << "Introduce un numero a continuacion : "<<endl; 
+    cin >> n ; 
+
+    comprobacion = calculo(n);
+
+    
+
+
+    return 0 ; 
+}
+
+bool calculo(int n1, int n2){
+
+    if ( n1 % n2 == 0) return true ;   
+    else  return false ; 
+
+}
