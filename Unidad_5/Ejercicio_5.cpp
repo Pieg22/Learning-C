14b2754a0c50b5f0f153b8447ee31aad47910301
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-30 16:29:04 +0100

    First structure

--- Unidad_5/Ejercicio_5.cpp
+++ Unidad_5/Ejercicio_5.cpp
@@ -3,3 +3,27 @@ anterior, implementa un programa que, usando funciones, sea capaz de
 inicializar libros con los datos que introduzca el usuario por teclado, mostrar libros
 e indicarte si un libro está escrito por un autor introducido por teclado por el
 usuario*/
+
+#include <iostream>
+using namespace std ; 
+
+int main (){
+    //variables + what we can put in while () ? 
+    do {
+
+    }while();
+    return 0; 
+}
+
+void nuevo_libro (){
+
+}
+
+void show_con_numero(){
+
+}
+
+void show_con_nombre(){
+
+}
+
