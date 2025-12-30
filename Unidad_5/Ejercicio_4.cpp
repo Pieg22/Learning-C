b362b10583c8328eeaa75ec2482c07e4073bc3ac
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-30 15:25:45 +0100

    Trying to complete de logic part with bucle and conditions

--- Unidad_5/Ejercicio_4.cpp
+++ Unidad_5/Ejercicio_4.cpp
@@ -10,20 +10,25 @@ int main (){
     bool primo  = true ; 
 
     cout << "Introduce un numero a continuacion : "<<endl; 
-    cin >> n ; 
+    cin >> n ;  // guardamos el n  , que es el que queremos comprobar 
+
+    if (n == 1 || n == 0){ 
+        primo = false ;         
+        cout << " El " << n << " no es primo "<<endl;
+     }
 
 
-    for(int i = 2 ; i < n ; i++) {
+   for(int i = 2 ; i < n ; i++) { // empezamos en  2 ya que 0 y 1 no nos ayuda para comprobar si es primo o no 
 
-        comprobacion = calculo(n,i);
-        if(comprobacion)
-        
-    }
+        comprobacion = calculo(n,i); // cuando la comprobación sea true querra decir que el resto es 0 
 
+        if (comprobacion == true ) primo = false ;// los numeros primos nunca tendrán un resto de 0 a no ser que sean 1 o el mismo .
 
-
-
+        return 
     
+    } 
+
+    cout << ""
 
 
     return 0 ; 
