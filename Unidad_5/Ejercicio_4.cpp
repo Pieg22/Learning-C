d7287e2446ff6b22b246b9e165093fc4c8062e09
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-30 15:55:24 +0100

    Done and clean

--- Unidad_5/Ejercicio_4.cpp
+++ Unidad_5/Ejercicio_4.cpp
@@ -3,17 +3,16 @@ número es primo o no.*/
 #include <iostream>
 using namespace std; 
 
-bool calculo(int, int) ; 
+bool calculo(int, int) ; // Prototipo de la funcion 
 
 int main (){
-    int n , n2 , comprobacion ; 
-    bool primo  = true ; 
+    int n , comprobacion ;  // Variable n para guardar el numero del user , y comprobacion para utilizarlo para correr la funcion
+    bool primo  = true ;  // bool flag , para determinar con seguridad el resultado 
 
-    cout << "Introduce un numero a continuacion : "<<endl; 
+    cout << "Introduce un numero a continuacion : "<<endl;  // pedimos n 
     cin >> n ;  // guardamos el n  , que es el que queremos comprobar 
 
-    if (n == 1 || n == 0){ 
-        primo = false ;         
+    if (n == 1 || n == 0){ // Si n == 1 o 0 , ya no hace falta que continuemos . 
         cout << " El " << n << " no es primo "<<endl;
      }
 
@@ -24,7 +23,7 @@ int main (){
         if (comprobacion == true ) primo = false ;// los numeros primos nunca tendrán un resto de 0 a no ser que sean 1 o el mismo .
     
     } 
-    if( primo == true) cout << " El numero "<< n << " es primo" ; 
+    if( primo == true) cout << " El numero "<< n << " es primo" ; // en este bloque condicional , la flag ya manda con totalidad y imprimimos el resultado adecuado en pantalla . 
     else cout << " El numero "<< n << " no es primo" ;
 
 
