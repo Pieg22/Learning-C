ebb6cc02f862fb15ce55dd5d1aadc75eeb405112
Pieg.22 <perebercerotrigell@gmail.com>
2025-12-30 17:03:36 +0100

    Trying to addapt all the stuf to the new structure

--- Unidad_5/Ejercicio_5.cpp
+++ Unidad_5/Ejercicio_5.cpp
@@ -7,8 +7,22 @@ usuario*/
 #include <iostream>
 using namespace std ; 
 
+const int max_libros = 100 ; // Constante para determinar la cantidad de huecos en la estanteria 
+
 int main (){
     //variables + what we can put in while () ? 
+
+      struct database {  // nombramos nuestra estructura de datos  para guardar la informacion sobre los libros
+        string title ;  // declaramos una variable string para el titulo
+        string writter ;  // variable autor 
+        int year ;  // variable de enteros para  el año
+        float precio ; //  variable con decimales para el precio
+    }libros[max_libros] ;  // Algo muy importante en este caso con las estructuras es necesario poner ; después del } , si no se rompe todo !
+    
+    
+
+
+
     do {
 
     }while();
@@ -16,14 +30,35 @@ int main (){
 }
 
 void nuevo_libro (){
+    cout << "A continuacion introduce los caracteres del nuevo libro"<<endl;
+    for( int i= 0 ; i < 4 ; i++){ // 4 porque el database se compone de 4 componentes ofc 
+        
+
+    }
+    
 
 }
 
-void show_con_numero(){
+ void show_con_numero(int registrer){
+     cout << "Introduce el numero de registro para encontrar el libro : " <<endl; // Pedimos al user el numero de registro en la memoria a la cual quiere acceder , si una prestablecida o una que haya creado recientemente
+     cin >> registrer ; // guardamos el valor de la pregunta anterior , para recoger el nº de registro y imprimir en consola la info del libro deseado 
+
+    cout << "Caracteristicas del libro : " << "\n" 
+            " - Titulo : " <<libros[registrer -1 ].title<< "\n" // registrer hace de int , para buscar el libro en la memoria 
+            " - Autor : " <<libros[registrer -1 ].writter<< "\n"
+            " - Año : " <<libros[registrer -1 ].year<< "\n"
+            " - Precio : " <<libros[registrer -1 ].precio<< endl; 
 
 }
 
-void show_con_nombre(){
+void show_con_nombre(string registrer){
+     cout << "Introduce el nombre del autor/a para encontrar el libro : " <<endl; // Pedimos al user el numero de registro en la memoria a la cual quiere acceder , si una prestablecida o una que haya creado recientemente
+     cin >> registrer ; // guardamos el valor de la pregunta anterior , para recoger el nº de registro y imprimir en consola la info del libro deseado 
 
+    cout << "Caracteristicas del libro : " << "\n" 
+            " - Titulo : " <<libros[registrer].title<< "\n" // registrer hace de int , para buscar el libro en la memoria 
+            " - Autor : " <<libros[registrer].writter<< "\n"
+            " - Año : " <<libros[registrer].year<< "\n"
+            " - Precio : " <<libros[registrer].precio<< endl; 
 }
 
