5cc2f06c4ee2c88b2b64bbdd79954edb5decbdf5
Pieg.22 <perebercerotrigell@gmail.com>
2026-01-04 17:04:44 +0100

    Copy all from ej.8

--- Unidad_5/Ejercicio_7.cpp
+++ Unidad_5/Ejercicio_7.cpp
@@ -1,3 +1,5 @@
 /*Implementa una función recursiva que muestre por pantalla en orden inverso las
 cifras de un número. Para ello, utiliza divisiones sucesivas por 10 y la operación
-módulo*/
+módulo
+Hay que implementar el programa entero, no solo la función.
+Los programas de los ejercicios 7 y 8 hacen lo mismo, pero uno usa una función recursiva (ej7) y el otro usa una función iterativa (ej8). Os recomiendo que hagáis primero el 8 y luego el 7.*/
