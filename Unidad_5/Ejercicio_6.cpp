e77fc452ac9aeb062dbfb3e740dc8d6b5aa46dc3
Pieg.22 <perebercerotrigell@gmail.com>
2026-01-04 11:27:51 +0100

    Structure done

--- Unidad_5/Ejercicio_6.cpp
+++ Unidad_5/Ejercicio_6.cpp
@@ -1,2 +1,30 @@
 /*Realiza una función que, dado un vector de N números enteros, devuelva la
-posición del último cero que encuentre en el vector.*/
+posición del último cero que encuentre en el vector.
+Hay que implementar el programa entero, no solo la función. Usad arrays. Los números se le deben pedir al usuario.*/
+
+
+#include <iostream>
+using namespace std ; 
+
+const int cantidad  = 100 ; // constante para la funcion y para el array
+
+//prototypes below 
+
+int main (){
+//variables 
+int  libreria_numeros[cantidad] ; 
+
+//prints
+//save numbers
+
+//get array and slide it betwen fuction
+
+
+    return 0; 
+}
+
+//fuction 
+int buscador_de_zero(int,int){
+
+}
+
