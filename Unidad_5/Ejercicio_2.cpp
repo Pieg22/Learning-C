15eebeb50f549052de3b1529c78d217937f9c41f
Pieg.22 <perebercerotrigell@gmail.com>
2026-01-06 10:21:40 +0100

    Commented and done

--- Unidad_5/Ejercicio_2.cpp
+++ Unidad_5/Ejercicio_2.cpp
@@ -4,28 +4,25 @@ y que el resto sea 0 ).*/
 #include <iostream>
 using namespace std ; 
 
-bool calculo(int n1, int n2) ; 
+bool calculo(int n1, int n2) ; //prototipo funcion con argumentos int 
 
 int main (){
-    int n1 , n2 ; 
+    int n1 , n2 ; // variables para recoger los dos numeros 
      
     cout << "Introduce dos numeros separados por un espacio para comprobar si son divisibles : "<<endl;
-    cin >> n1 >> n2 ; 
+    cin >> n1 >> n2 ;  // guardamos los numeros 
 
-    bool resultado = calculo (n1,n2);
+    bool resultado = calculo (n1,n2); // creamos una variable boleana , dandole como valor la función con los dos numeros guardados previamente
 
-    if ( resultado == true ) cout << "Los numeros son divisibles "<<endl; 
-    else cout<< "Los numeros no son divisibles. "<<endl; 
-
-
-    
+    if ( resultado == true ) cout << "Los numeros son divisibles "<<endl; // si la funcion devuelve true , es ejecuta el if 
+    else cout<< "Los numeros no son divisibles. "<<endl;  // si return false , se imprime esta linea . 
 
     return 0 ; 
 }
 
-bool calculo(int n1, int n2){
+bool calculo(int n1, int n2){ // funcion boleana , para no crear una void o una int y tener que hacer una flag dentro , directamente la creamos bool . 
 
-    if ( n1 % n2 == 0) return true ;   
-    else  return false ; 
+    if ( n1 % n2 == 0) return true ;   // Si el resto de la division de los numeros es 0 querra decir que esos dos numeros si son divisibles , de lo contrario , siempre quedara algun otro n real.
+    else  return false ; // Si el if no se cumple directamente enviamos un false . 
 
 }
